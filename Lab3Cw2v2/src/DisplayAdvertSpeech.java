396fb784a8ce8a7e98ad8e5a637cfad07fbe0220
ogurecze <162505048+ogurecze@users.noreply.github.com>
2024-06-13 09:51:03 +0200

    Delete Lab3Cw2v2 directory

--- Lab3Cw2v2/src/DisplayAdvertSpeech.java
+++
@@ -1,6 +0,0 @@
-public class DisplayAdvertSpeech implements DisplayAdvert{
-    @Override
-    public void display(String advert) {
-        System.out.println("Reading advert: " + advert);
-    }
-}
